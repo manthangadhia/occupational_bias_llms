0bf903f6bf9e015efba235cd3eb7234ada6af7d6
manthangadhia <54841046+manthangadhia@users.noreply.github.com>
2026-03-11 20:35:35 +0530

    Updated to avoid eagerly loading utils with torch dependencies.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,8 @@
 # utils/__init__.py
 from .prompt_loader import load_prompts_for_model, get_prompt_files
-from .model_utils import load_model, generate, generate_with_entropy, cleanup_model
 from .load_json_data import load_json_data, save_dataframes
+
+try:
+    from .model_utils import load_model, generate, generate_with_entropy, cleanup_model
+except ImportError:
+    print("WARNING UTILS: model_utils could not be loaded; ensure that torch is installed if you intend to use it.")
