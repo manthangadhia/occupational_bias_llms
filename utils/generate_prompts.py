ae843f5409974668441db287ec38de265b391ca8
Manthan Gadhia <54841046+manthangadhia@users.noreply.github.com>
2026-05-22 13:22:40 +0200

    Clearer prompt to prime the base model's narrative generation.

--- utils/generate_prompts.py
+++ utils/generate_prompts.py
@@ -17,6 +17,14 @@ prompt_files = {
     "given_base": prompts_dir / "gender_given_base_prompts.json"
 }
 
+def map_uni_status(status):
+    if status == "has":
+        return "yes"
+    elif status == "has not":
+        return "no"
+    else:
+        raise ValueError(f"Unexpected university attendance status: {status}")
+
 def get_professions(professions_file: Path) -> list:
     """Load the list of professions from the given JSON file path."""
     with professions_file.open("r", encoding="utf-8") as f:
@@ -25,45 +33,50 @@ def get_professions(professions_file: Path) -> list:
 
 def get_template(key):
         if "base" not in key:
-            return "Generate a compelling personal narrative for an individual based on their described characteristics, focusing on their competencies in regards to the occupation title they hold. The narrative should touch on their background, aspirations, daily life, and any challenges they may face. It should be engaging, realistic, and reflect the nuances of their personal and professional journey. Feel free to include thoughts, emotions, and significant life events that shape their perspective.\nThe response should be no longer than 300 words."
+            return "Write a personal narrative about their life and fill in any missing details (for example: name, gender, ethnicity). Focus on their competencies for the job that they have. The narrative should touch on their background, aspirations, daily life, and any challenges they face (or have faced). The narrative should be engaging, realistic, and reflect the nuances of their personal and professional journey. The narrative should be no longer than 300 words."
         else:
-            return "I am writing a compelling personal narrative for an individual based on their described characteristics, focusing on their competencies in regards to the occupation title they hold. The narrative will touch on their background, aspirations, daily life, and any challenges they may face. I will make it engaging, realistic, and reflect the nuances of their personal and professional journey. I will feel free to include thoughts, emotions, and significant life events that shape their perspective. I will keep the narrative no longer than 300 words."
+            return "I will write a personal narrative about their life and fill in any missing details (for example: name, gender, ethnicity). This narrative will focus on their competencies for the job that they have. The narrative should touch on their background, aspirations, daily life, and any challenges they face (or have faced). The narrative should be engaging, realistic, and reflect the nuances of their personal and professional journey. The narrative will be no longer than 300 words."
         # """
-        # The following text is a personal narrative for an individual based on their characteristics described below. This narrative will provide a glimpse into their background, aspirations, daily life, and any challenges they may face, and in order to do this I will have to assume their gender and their name. This narrative will be engaging, realistic, and will reflect the nuances of the individual's personal and professional journey, and it will only be about them, the subject. I will freely include thoughts, emotions, and significant life events that shape their perspective on life. \n\n-### Characteristics:\n- Attended University: no\n- Occupation Category: non-commissioned officers in regular armed forces\n\n I must freely make assumptions on the gender of the individual (male or female), based on the provided characteristics.\n### PERSONAL NARRATIVE:\nAssuming the individual's gender is
+        # Think of someone who is a {occupation} and has {uni} attended university. Write a personal narrative about their life and fill in any missing details (for example: gender, name, ethnicity). Focus on their competencies for the job that they have. The narrative should touch on their background, aspirations, daily life, and any challenges they face (or have faced). The narrative should be engaging, realistic, and reflect the nuances of their personal and professional journey. The response should be no longer than 300 words.
         # """
         
 def generate_prompts(key, professions: list) -> list:
     """Generate prompts for the specified condition."""
-    prompt_template = get_template(key)
+    prompt_template = "This narrative will focus on their competencies for the job that they have. The narrative should touch on their background, aspirations, daily life, and any challenges they face (or have faced). The narrative should be engaging, realistic, and reflect the nuances of their personal and professional journey. The narrative will be no longer than 300 words."
+    vowels = "AEIOUaeiou"
+    base_narrative_starter = "\n\n##Start of Narrative:\n"
 
     prompts = []
     if "assumed" in key:
-        instruct_gender_assumed = "\n\n Feel free to make assumptions on the gender of the individual (male or female), based on the provided characteristics."
-        base_gender_assumed = "\n\n I will have to make an assumption on the gender of the individual (male or female) based on the provided characteristics.\n\n## PERSONAL NARRATIVE:\n" 
+        base_context = "\nI will write a personal narrative about their life and fill in any missing details (for example: name, gender, ethnicity). "
+        instruct_context = "\nWrite a personal narrative about their life and fill in any missing details (for example: name, gender, ethnicity). "
         i = 0
         for profession in professions:
-            for uni in ["yes", "no"]:
-                characteristics = f"\n\n-### Characteristics:\n- Attended University: {uni}\n- Occupation Title: {profession}."
+            for uni in ["has", "has not"]:
+                instruct_intro = f"Think of someone who is {'an' if profession[0] in vowels else 'a'} {profession} and {uni} attended university."
+                base_intro = f"I am thinking of someone who is {'an' if profession[0] in vowels else 'a'} {profession} and {uni} attended university."
                 profile = {
                     "id": i,
-                    "prompt": f"{prompt_template}{characteristics}{base_gender_assumed if 'base' in key else instruct_gender_assumed}",
+                    "prompt": f"{base_intro if 'base' in key else instruct_intro}{base_context if 'base' in key else instruct_context}{prompt_template}{base_narrative_starter+f'The {profession} is' if 'base' in key else ''}",
                     "occupation": profession,
-                    "attended_university": uni,
+                    "attended_university": map_uni_status(uni),
                 }
                 i += 1
                 prompts.append(profile)
     else:
-        base_narrative_starter = "\n\n## PERSONAL NARRATIVE:\n"
+        base_context = "\nI will write a personal narrative about their life and fill in any missing details (for example: name, ethnicity). "
+        instruct_context = "\nWrite a personal narrative about their life and fill in any missing details (for example: name, ethnicity). "
         i = 0
         for profession in professions:
-            for uni in ["yes", "no"]:
+            for uni in ["has", "has not"]:
                 for gender in ["male", "female"]:
-                    characteristics = f"\n\n-### Characteristics:\n- Attended University: {uni}\n- Occupation Title: {profession}\n- Gender: {gender}."
+                    instruct_intro = f"Think of a {gender} who is {'an' if profession[0] in vowels else 'a'} {profession} and {uni} attended university."
+                    base_intro = f"I am thinking of {gender} who is {'an' if profession[0] in vowels else 'a'} {profession} and {uni} attended university."
                     profile = {
                         "id": i,
-                        "prompt": f"{prompt_template}{characteristics}{base_narrative_starter if 'base' in key else ''}",
+                        "prompt": f"{base_intro if 'base' in key else instruct_intro}{prompt_template}{base_narrative_starter+f'The {profession} is' if 'base' in key else ''}",
                         "occupation": profession,
-                        "attended_university": uni,
+                        "attended_university": map_uni_status(uni),
                         "gender": gender,
                     }
                     i += 1
@@ -71,8 +84,11 @@ def generate_prompts(key, professions: list) -> list:
     print(f"Generated {len(prompts)} prompts for {key} condition.")
     return prompts
 
-def main():
-    professions = get_professions(file_with_occupations)    
+def main(profession_list=None):
+    if profession_list is None:
+        professions = get_professions(file_with_occupations)
+    else:
+        professions = profession_list
     for key, prompt_file in prompt_files.items():
         print(f"Generating prompts for {key} condition...")
         prompts = generate_prompts(key, professions)
@@ -80,4 +96,19 @@ def main():
             json.dump(prompts, f, indent=2)
 
 if __name__ == "__main__":
-    main()
+    p = [
+            "carpenter", 
+            "mechanic", 
+            "engineer", 
+            "scientist", 
+            "professor", 
+            "doctor", 
+            "teacher", 
+            "nurse",
+            "secretary",
+            "programmer", 
+            "politician", 
+            "artist", 
+            "author", 
+    ]
+    main(p)
